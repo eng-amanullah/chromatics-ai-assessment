f47c90a84a98fb0f7ed6b370e73ebe010173adec
amanullah <amanullah.sarker@amiprobashi.com>
2025-01-24 15:30:06 +0600

    file : theme
    changes:
    - move to base layer

--- app/src/main/java/com/amanullah/chromaticsaiassessment/base/theme/Theme.kt
+++ app/src/main/java/com/amanullah/chromaticsaiassessment/base/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.amanullah.chromaticsaiassessment.ui.theme
+package com.amanullah.chromaticsaiassessment.base.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
--- app/src/main/java/com/amanullah/chromaticsaiassessment/base/theme/Type.kt
+++ app/src/main/java/com/amanullah/chromaticsaiassessment/base/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.amanullah.chromaticsaiassessment.ui.theme
+package com.amanullah.chromaticsaiassessment.base.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
